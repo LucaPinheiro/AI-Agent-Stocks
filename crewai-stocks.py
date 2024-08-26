add20ef564e533f1b34da8e5b70067f9410f5550
LucaPinheiro <lucapgomes11@gmail.com>
2024-08-25 22:35:16 -0300

    chore: Update stock analysis write-up format and language

--- crewai-stocks.py
+++ crewai-stocks.py
@@ -1,4 +1,4 @@
-import json 
+import json
 import os
 from datetime import datetime
 
@@ -12,15 +12,18 @@ from langchain_community.tools import DuckDuckGoSearchResults
 
 import streamlit as st
 
-# CRIANDO YAHOO FINANCE TOOL 
+# CRIANDO YAHOO FINANCE TOOL
+
+
 def fetch_stock_price(ticket):
     stock = yf.download(ticket, start="2023-08-08", end="2024-08-08")
     return stock
 
+
 yahoo_finance_tool = Tool(
-    name = "Yahoo Finance Tool",
-    description = "Fetches stocks prices for {ticket} from the last year about a specific company from Yahoo Finance API",
-    func= lambda ticket: fetch_stock_price(ticket)
+    name="Yahoo Finance Tool",
+    description="Fetches stocks prices for {ticket} from the last year about a specific company from Yahoo Finance API",
+    func=lambda ticket: fetch_stock_price(ticket)
 )
 
 # IMPORTANDO OPENAI LLM - GPT
@@ -29,14 +32,14 @@ llm = ChatOpenAI(model="gpt-4o")
 
 
 stockPriceAnalyst = Agent(
-    role= "Senior stock price Analyst",
+    role="Senior stock price Analyst",
     goal="Find the {ticket} stock price and analyses trends",
     backstory="""You're highly experienced in analyzing the price of an specific stock
     and make predictions about its future price.""",
     verbose=True,
-    llm= llm,
-    max_iter= 5,
-    memory= True,
+    llm=llm,
+    max_iter=5,
+    memory=True,
     tools=[yahoo_finance_tool],
     allow_delegation=False
 )
@@ -46,18 +49,18 @@ stockPriceAnalyst = Agent(
 
 
 getStockPrice = Task(
-    description= "Analyze the stock {ticket} price history and create a trend analyses of up, down or sideways",
-    expected_output = """" Specify the current trend stock price - up, down or sideways. 
+    description="Analyze the stock {ticket} price history and create a trend analyses of up, down or sideways",
+    expected_output="""" Specify the current trend stock price - up, down or sideways. 
     eg. stock= 'APPL, price UP'
 """,
-    agent= stockPriceAnalyst
+    agent=stockPriceAnalyst
 )
 
 
 # In[7]:
 
 
-# IMPORTANT A TOOL DE SEARCH 
+# IMPORTANT A TOOL DE SEARCH
 search_tool = DuckDuckGoSearchResults(backend='news', num_results=10)
 
 
@@ -65,7 +68,7 @@ search_tool = DuckDuckGoSearchResults(backend='news', num_results=10)
 
 
 newsAnalyst = Agent(
-    role= "Stock News Analyst",
+    role="Stock News Analyst",
     goal="""Create a short summary of the market news related to the stock {ticket} company. Specify the current trend - up, down or sideways with
     the news context. For each request stock asset, specify a numbet between 0 and 100, where 0 is extreme fear and 100 is extreme greed.""",
     backstory="""You're highly experienced in analyzing the market trends and news and have tracked assest for more then 10 years.
@@ -76,9 +79,9 @@ newsAnalyst = Agent(
     You consider also the source of the news articles. 
     """,
     verbose=True,
-    llm= llm,
-    max_iter= 10,
-    memory= True,
+    llm=llm,
+    max_iter=10,
+    memory=True,
     tools=[search_tool],
     allow_delegation=False
 )
@@ -88,20 +91,20 @@ newsAnalyst = Agent(
 
 
 get_news = Task(
-    description= f"""Take the stock and always include BTC to it (if not request).
+    description=f"""Take the stock and always include BTC to it (if not request).
     Use the search tool to search each one individually. 
 
     The current date is {datetime.now()}.
 
     Compose the results into a helpfull report""",
-    expected_output = """"A summary of the overall market and one sentence summary for each request asset. 
+    expected_output=""""A summary of the overall market and one sentence summary for each request asset. 
     Include a fear/greed score for each asset based on the news. Use format:
     <STOCK ASSET>
     <SUMMARY BASED ON NEWS>
     <TREND PREDICTION>
     <FEAR/GREED SCORE>
 """,
-    agent= newsAnalyst
+    agent=newsAnalyst
 )
 
 
@@ -109,19 +112,19 @@ get_news = Task(
 
 
 stockAnalystWrite = Agent(
-    role = "Senior Stock Analyts Writer",
-    goal= """"Analyze the trends price and news and write an insighfull compelling and informative 3 paragraph long newsletter based on the stock report and price trend. """,
-    backstory= """You're widely accepted as the best stock analyst in the market. You understand complex concepts and create compelling stories
+    role="Senior Stock Analyts Writer",
+    goal=""""Analyze the trends price and news and write an insighfull compelling and informative 3 paragraph long newsletter based on the stock report and price trend. """,
+    backstory="""You're widely accepted as the best stock analyst in the market. You understand complex concepts and create compelling stories
     and narratives that resonate with wider audiences. 
 
     You understand macro factors and combine multiple theories - eg. cycle theory and fundamental analyses. 
     You're able to hold multiple opinions when analyzing anything.
 """,
-    verbose = True,
+    verbose=True,
     llm=llm,
-    max_iter = 5,
+    max_iter=5,
     memory=True,
-    allow_delegation = True
+    allow_delegation=True
 )
 
 
@@ -129,22 +132,22 @@ stockAnalystWrite = Agent(
 
 
 writeAnalyses = Task(
-    description = """Use the stock price trend and the stock news report to create an analyses and write the newsletter about the {ticket} company
+    description="""Use the stock price trend and the stock news report to create an analyses and write the newsletter about the {ticket} company
     that is brief and highlights the most important points.
     Focus on the stock price trend, news and fear/greed score. What are the near future considerations?
     Include the previous analyses of stock trend and news summary.
 """,
-    expected_output= """"An eloquent 3 paragraphs newsletter formated as markdown in an easy readable manner. It should contain:
+    expected_output=""""An eloquent 3 paragraphs newsletter formated as markdown in an easy readable manner. It should contain:
 
     - 3 bullets executive summary 
     - Introduction - set the overall picture and spike up the interest
     - main part provides the meat of the analysis including the news summary and fead/greed scores
     - summary - key facts and concrete future trend prediction - up, down or sideways.
     
-    write in pt-br and use markdown format.
+    write in pt-br.
 """,
-    agent = stockAnalystWrite,
-    context = [getStockPrice, get_news]
+    agent=stockAnalystWrite,
+    context=[getStockPrice, get_news]
 )
 
 
@@ -152,10 +155,10 @@ writeAnalyses = Task(
 
 
 crew = Crew(
-    agents = [stockPriceAnalyst, newsAnalyst, stockAnalystWrite],
-    tasks = [getStockPrice, get_news, writeAnalyses],
-    verbose = 2,
-    process= Process.hierarchical,
+    agents=[stockPriceAnalyst, newsAnalyst, stockAnalystWrite],
+    tasks=[getStockPrice, get_news, writeAnalyses],
+    verbose=2,
+    process=Process.hierarchical,
     full_output=True,
     share_crew=False,
     manager_llm=llm,
@@ -169,12 +172,12 @@ with st.sidebar:
 
     with st.form(key='research_form'):
         topic = st.text_input("Select the ticket")
-        submit_button = st.form_submit_button(label = "Run Research")
+        submit_button = st.form_submit_button(label="Run Research")
 if submit_button:
     if not topic:
         st.error("Please fill the ticket field")
     else:
-        results= crew.kickoff(inputs={'ticket': topic})
+        results = crew.kickoff(inputs={'ticket': topic})
 
         st.subheader("Results of research:")
         st.write(results['final_output'])
